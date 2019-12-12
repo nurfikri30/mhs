8185203fc2d8aab07a2a39f125e800f182e5548d
nurfikri30 <48448878+nurfikri30@users.noreply.github.com>
2019-12-12 11:31:00 +0700

    Add files via upload

---
+++ PerpustakaanAppMVC/PerpustakaanAppMVC/View/FrmLaporanMahasiswa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+using PerpustakaanAppMVC.Model.Entity;
+using PerpustakaanAppMVC.Controller;
+
+namespace PerpustakaanAppMVC.View
+{
+    public partial class FrmLaporanMahasiswa : Form
+    {
+        public FrmLaporanMahasiswa()
+        {
+            InitializeComponent();
+            InisialisasiListView();
+        }
+
+        // atur kolom listview
+        private void InisialisasiListView()
+        {
+            lvwMahasiswa.View = System.Windows.Forms.View.Details;
+            lvwMahasiswa.FullRowSelect = true;
+            lvwMahasiswa.GridLines = true;
+
+            lvwMahasiswa.Columns.Add("No.", 35, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Npm", 91, HorizontalAlignment.Center);
+            lvwMahasiswa.Columns.Add("Nama", 350, HorizontalAlignment.Left);
+            lvwMahasiswa.Columns.Add("Angkatan", 80, HorizontalAlignment.Center);
+        }
+    }
+}
